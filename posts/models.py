06ed1d86336c4d102ea6593f4830db888d1baaf3
Daniil Isaverdov <daniil.isaverdov@gmail.com>
2026-04-16 17:47:19 +0300

    feat(posts):  add likes_count and comments_count fields

--- posts/models.py
+++ posts/models.py
@@ -7,8 +7,8 @@ class Post(models.Model):
     title = models.CharField(max_length=20)
     content = models.TextField(max_length=500)
     media = models.ImageField(upload_to='media/%Y%m%d', blank=True)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return f"Автор: {self.author}, Заголовок поста: {self.title}"
--- posts/serializers.py
+++ posts/serializers.py
@@ -10,14 +10,30 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = ("__all__")
         read_only_fields = ("author", "post",)  
 
+
 class PostSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
     comments = CommentSerializer(many=True, read_only=True)
+
+    likes_count = serializers.SerializerMethodField()
+    is_liked = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content', 'media', 'created_at', 'updated_at', 'author', 'comments']
+        fields = ['id', 'title', 'content', 'media', 'created_at', 'updated_at', 'author', 'comments', 'is_liked', 'likes_count', 'comments_count']
         read_only_fields = ("author",)
+    
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+    def get_is_liked(self, obj):
+        request = self.context.get("request")
 
+        if request and request.user.is_authenticated:
+            return obj.likes.filter(user=request.user).exists()
+
+        return False
+    def get_comments_count(self, obj):
+        return obj.comments.count()
 class LikeSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     class Meta:

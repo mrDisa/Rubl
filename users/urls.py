2be9516eae64e7908eeed11b84182d94c59fa58e
Bogdan Maksimenko <bogdanmaksimenko51@gmail.com>
2026-04-20 16:39:35 +0300

    feat: connect frontend registration form to DRF API

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .views import UserAPIList, UserDetailView, UserMeView, UserRegisterAPIView
+
 urlpatterns = [
-    path('register/', UserRegisterAPIView.as_view()),
+    path('signup/', TemplateView.as_view(template_name='users/register.html'), name='signup_page'),
+    path('register/', UserRegisterAPIView.as_view(), name='api_register'),
     path('', UserAPIList.as_view()),
     path('<int:pk>/', UserDetailView.as_view()),
     path('me/', UserMeView.as_view()),
